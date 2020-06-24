d1cb866712ad551d2ab3c00a43d30c956ceda246
Philipp Behrens <31588467+pbeh@users.noreply.github.com>
2020-06-24 15:24:42 +0200

    Added another query

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -6,6 +6,9 @@ public class QueryProcessor {
         if (query.toLowerCase().contains("romeo and juliet")) {
             return "William Shakespeare";
         }
+        if (query.toLowerCase().contains("hitchhiker's guide to the galaxy")) {
+            return "Douglas Adams";
+        }
         return "";
     }
 }
